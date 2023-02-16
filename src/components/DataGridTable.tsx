240d09340a900160bb5eea1ba2286dc6b293e618
Abiraman K <abiramancit@gmail.com>
2023-02-16 20:05:00 +0530

    [Abiraman K] Adds DataGridTable component

---
+++ src/components/DataGridTable.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { Box } from "@mui/material";
+import { DataGrid, GridColDef, GridEventListener } from "@mui/x-data-grid";
+
+interface IDataGridTableProps {
+  rows: Array<any>;
+  columns: GridColDef[];
+  pageSize: number;
+  rowsPerPageOptions: Array<number>;
+  disableSelectionOnClick: boolean;
+  newEditingApi: boolean;
+  tableHeight: number | string;
+  loading: boolean;
+  onRowClick: Function;
+}
+
+export default function DataGridTable(props: IDataGridTableProps) {
+  const handleRowClickEvent: GridEventListener<"rowClick"> = (
+    params, // GridRowParams
+    event, // MuiEvent<React.MouseEvent<HTMLElement>>
+    details // GridCallbackDetails
+  ) => {
+    props?.onRowClick(params?.row);
+  };
+
+  return (
+    <React.Fragment>
+      <Box sx={{ height: props?.tableHeight, width: "100%" }}>
+        <DataGrid
+          rows={props?.rows}
+          columns={props?.columns}
+          pageSize={props?.pageSize}
+          rowsPerPageOptions={props?.rowsPerPageOptions}
+          disableSelectionOnClick={props?.disableSelectionOnClick}
+          experimentalFeatures={{ newEditingApi: props?.newEditingApi }}
+          paginationMode={"client"}
+          loading={props?.loading}
+          onRowClick={handleRowClickEvent}
+        />
+      </Box>
+    </React.Fragment>
+  );
+}
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,6 @@
 import AccountMenu from "./AccountMenu";
 import Copyright from "./Copyright";
+import DataGridTable from "./DataGridTable";
 import Title from "./Title";
 
-export { Copyright, AccountMenu, Title };
+export { Copyright, AccountMenu, Title, DataGridTable };
